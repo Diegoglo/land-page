68e51358b27274c060ef7e47f1a5720742a61700
pablo-data <pablo.acu.ramirez@gmail.com>
2021-07-30 13:07:38 -0400

    refactor(app) : comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,11 +13,11 @@ const routes: Routes = [
     loadChildren: () => import('./visitor/visitor.module')
       .then(m => m.VisitorModule)
   },*/
-  {
+  /*{
     path: 'admin',
     loadChildren: () => import('./admin/admin.module')
       .then(m => m.AdminModule)
-  },
+  }, */
 ];
 
 @NgModule({
